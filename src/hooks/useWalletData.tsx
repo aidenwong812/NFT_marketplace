c073bdb7a43fb6b97e538234c31a5cfd69c8ac4d
hiccup2735 <yujinhirose2735@gmail.com>
2024-04-17 14:27:08 +1000

    error handle

--- src/hooks/useWalletData.tsx
+++ src/hooks/useWalletData.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 
 const useWalletData = () => {
-  const networkValue = process.env.NEXT_PUBLIC_NETWORK.toString();
+  const networkValue = process.env.NEXT_PUBLIC_NETWORK.toString() || "devnet";
   const [network, setNetwork] = useState(networkValue);
   const [walletID, setWalletID] = useState("");
   const [connStatus, setConnStatus] = useState(false);
