84fce541ac98bb27a0945944fcb61768db5795fc
hiccup2735 <yujinhirose2735@gmail.com>
2024-04-17 12:37:03 +1000

    redirect marketplace first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.push("/nfts");
+    router.push("/marketplace");
   }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
--- src/components/layout/Sider/sider/index.tsx
+++ src/components/layout/Sider/sider/index.tsx
@@ -7,32 +7,12 @@ const SiderList1 = (props: any) => {
     "4dJ6QHdisXwcFVjADr8dS7BqHfDUfuzc6pzDoTEgGDQi",
     "FhvzCMoiTq8JHZuKwiPsGPgVtQjR8Z4KJ71r55h2tgqC",
     "CBDspbBz3Xy6MYyURfGVzqMg7rS8EsQzBy3hf7UbR7uv",
-  ]
-  const { walletID } = useWallet()
-  
+  ];
+  const { walletID } = useWallet();
+
   return (
     <>
       <ul className="text-[#D4D4D4] text-md mt-[10px] h-full overflow-auto">
-        <ListComponent
-          _name="My NFTs"
-          _icon="/icon/NFT.svg"
-          _clickedIcon="/icon/NFT_blue.svg"
-          _url="/nfts"
-          pathname={props.pathname}
-          siderWidth={props.siderWidth}
-        />
-        {
-          admins.includes(walletID) && (
-            <ListComponent
-              _name="Create NFT"
-              _icon="/icon/create_icon_white.svg"
-              _clickedIcon="/icon/create_icon_blue.svg"
-              _url="/create-nft"
-              pathname={props.pathname}
-              siderWidth={props.siderWidth}
-            />
-          )
-        }
         <ListComponent
           _name="Marketplace"
           _icon="/icon/marketplace.svg"
@@ -41,6 +21,24 @@ const SiderList1 = (props: any) => {
           pathname={props.pathname}
           siderWidth={props.siderWidth}
         />
+        <ListComponent
+          _name="My NFTs"
+          _icon="/icon/NFT.svg"
+          _clickedIcon="/icon/NFT_blue.svg"
+          _url="/nfts"
+          pathname={props.pathname}
+          siderWidth={props.siderWidth}
+        />
+        {admins.includes(walletID) && (
+          <ListComponent
+            _name="Create NFT"
+            _icon="/icon/create_icon_white.svg"
+            _clickedIcon="/icon/create_icon_blue.svg"
+            _url="/create-nft"
+            pathname={props.pathname}
+            siderWidth={props.siderWidth}
+          />
+        )}
         <ListComponent
           _name="Transfer"
           _icon="/icon/transfer_white.svg"
